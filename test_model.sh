abe3f317f94dd98c627b562680c81ef9c58e2bdd
ChiaYi-LIN <b04704016@ntu.edu.tw>
2023-04-05 02:03:27 +0800

    set library path for 1080ti server

--- test_model.sh
+++ test_model.sh
@@ -1,4 +1,4 @@
-export LD_LIBRARY_PATH=/service/software/cuda/cuda-10.2/lib64:/service/software/cudnn/cudnn-11.3-linux-x64-v8.2.1.32/cuda/lib64:/tmp2/linchiayi/mmdeploy/TensorRT-8.2.3.0-cuda10.2/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/service/software/cuda/cuda-10.2/lib64:/tmp2/linchiayi/cuda/lib64:/tmp2/linchiayi/TensorRT-8.2.3.0-cuda10.2/lib:$LD_LIBRARY_PATH
 # export LD_LIBRARY_PATH=/service/software/cuda/cuda-11.3/lib64:/service/software/cudnn/cudnn-11.3-linux-x64-v8.2.1.32/cuda/lib64:/tmp2/linchiayi/mmdeploy/TensorRT-8.2.3.0-cuda11.4/lib:$LD_LIBRARY_PATH
 
 deploy_cfg="mmdeploy/configs/mmseg/segmentation_tensorrt_static-512x1024.py"
