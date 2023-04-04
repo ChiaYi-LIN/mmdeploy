bb1977d0967489f6be9beab478d2f664bbd344cd
ChiaYi-LIN <b04704016@ntu.edu.tw>
2023-04-05 01:37:34 +0800

    add cudnn for cuda 10.2

--- deploy_model.sh
+++ deploy_model.sh
@@ -1,9 +1,9 @@
-export LD_LIBRARY_PATH=/service/software/cuda/cuda-10.2/lib64:/service/software/cudnn/cudnn-11.3-linux-x64-v8.2.1.32/cuda/lib64:/tmp2/linchiayi/mmdeploy/TensorRT-8.2.3.0-cuda10.2/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/service/software/cuda/cuda-10.2/lib64:/tmp3/linchiayi/cuda/lib64:/tmp3/linchiayi/TensorRT-8.2.3.0-cuda10.2/lib:$LD_LIBRARY_PATH
 # export LD_LIBRARY_PATH=/service/software/cuda/cuda-11.3/lib64:/service/software/cudnn/cudnn-11.3-linux-x64-v8.2.1.32/cuda/lib64:/tmp2/linchiayi/mmdeploy/TensorRT-8.2.3.0-cuda11.4/lib:$LD_LIBRARY_PATH
 
 deploy_cfg="mmdeploy/configs/mmseg/segmentation_tensorrt_static-512x1024.py"
 model_cfg="mmsegmentation/configs/entextnet/entextnet-stdc1_4x12_512x1024_scale0.5_160k_cityscapes.py"
-checkpoint="mmsegmentation/work_dirs/entextnet-stdc1_4x12_512x1024_scale0.5_160k_cityscapes/iter_16000.pth"
+checkpoint="mmsegmentation/work_dirs/entextnet-stdc1_4x12_512x1024_scale0.5_160k_cityscapes/iter_32000.pth"
 img="mmsegmentation/data/cityscapes/leftImg8bit/val/frankfurt/frankfurt_000000_000294_leftImg8bit.png"
 work_dir="mmdeploy/mmdeploy_model/entextnet"
 
